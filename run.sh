1aaaba33c64508e80236e7d4058af32065560035
Kevin Espinoza <k.espinoza1012@csu.fullerton.edu>
2021-02-15 00:11:05 -0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+# Lexical Analyzer
+# run.sh
+# 323-02
+
+# Written By: Kevin Espinoza
+#             Andres Jaramillo
+#             Jaime Lambrecht
+#             Steven Tran
+#             est_jan -- replace w real name
+
+
+
+
+# Compile 
+g++ -o main.o main.cpp -std=c++17
+
+# Clean up files
+rm *.o
+
