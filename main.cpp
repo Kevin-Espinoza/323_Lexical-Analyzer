10d81a248447d90fd1274f9d00a06f542e850d15
Jamie Lambrecht <mjlambrecht@yahoo.com>
2021-03-07 19:21:58 -0800

    readToFile() renamed to lexer()

--- main.cpp
+++ main.cpp
@@ -14,18 +14,18 @@
 int main(int argc, char *argv[]) 
 {
     // Declare parser as a pointer so that we can choose which constructor to call based on command-line arguments
-    Parser *parser; 
+    Parser parser; 
 
     if (argc == 1) 
     {
         // Use default file name from default constructor
-        parser = new Parser();
+        parser = Parser();
     } 
     else if (argc == 2) 
     {
         // Pass filename from command-line argument to constructor
         std::string filename = argv[1];
-        parser = new Parser(filename);
+        parser = Parser(filename);
     } else {
         // Too many arguments from command-line, give user error and exit.
         std::cerr << "Too many arguments!";
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     }
 
     // Begin lexical analysis
-    parser->readFromFile();
+    parser.lexer();
 
     return 0;
 }
